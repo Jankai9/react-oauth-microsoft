0002b9be6d833dca61eb0829023e03a3ac496332
Janne Kaipala <janne.kaipala@hiq.fi>
2024-02-17 12:32:49 +0200

    paranneltu

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MicrosoftLogin from 'react-microsoft-login'
 
 export default (props) => {
+  const [tiedot, asetaTiedot] = useState(undefined)
+
   const authHandler = (err, data) => {
-    console.log('saadun vastauksen err:')
-    console.log(err)
-    console.log('saadun vastauksen data:')
-    console.log(data)
+    if (err) console.log('kirjautumisessa tuli virhe:', err)
+    else {
+      console.log('kirjautuminen onnistui:', data)
+      asetaTiedot(data)
+    }
   }
   const CLIENT_ID = '9b3182b6-6b17-4931-8941-414c08968163'
   return (
-    <MicrosoftLogin
-      clientId={CLIENT_ID}
-      redirectUri='http://localhost:3001'
-      authCallback={authHandler}
-    />
+    <div>
+      {tiedot ? (
+        <div>
+          <p>Käyttäjä on kirjautunut: {tiedot.account.name}</p>
+          <p>Nimi: {tiedot.account.username}</p>
+        </div>
+      ) : (
+        <MicrosoftLogin
+          clientId={CLIENT_ID}
+          redirectUri='http://localhost:3001'
+          authCallback={authHandler}
+        />
+      )}
+    </div>
   )
 }
