1b0371ec7193bed36f4f440cf18b921d0af7fb79
Janne Kaipala <janne.kaipala@hiq.fi>
2024-02-17 13:29:19 +0200

    save

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -4,6 +4,7 @@ import MicrosoftLogin from 'react-microsoft-login'
 const CLIENT_ID = '9b3182b6-6b17-4931-8941-414c08968163'
 
 export default () => {
+  const [msalInstance, onMsalInstanceChange] = useState()
   const [tiedot, asetaTiedot] = useState(undefined)
   const lsTiedot = JSON.parse(localStorage.getItem('data'))
   console.log('lsTiedot:', lsTiedot)
@@ -12,10 +13,18 @@ export default () => {
     if (err) console.log('kirjautumisessa tuli virhe:', err)
     else {
       console.log('kirjautuminen onnistui:', data)
+      console.log('msal:', msal)
+      onMsalInstanceChange(msal)
       asetaTiedot(data)
       localStorage.setItem('data', JSON.stringify(data))
     }
   }
+
+  const logoutHandler = () => {
+    localStorage.clear('data')
+    msalInstance.logout()
+  }
+
   return (
     <div>
       {lsTiedot ? (
@@ -26,12 +35,14 @@ export default () => {
             Vanhentuu: {new Date(lsTiedot.expiresOn).toLocaleDateString()}:
             {new Date(lsTiedot.expiresOn).toLocaleTimeString()}}
           </p>
+          <button onClick={logoutHandler}>Logout</button>
         </div>
       ) : (
         <MicrosoftLogin
           clientId={CLIENT_ID}
-          redirectUri='http://localhost:3001'
           authCallback={authHandler}
+          redirectUri='http://localhost:3001'
+          postLogoutRedirectUri='http://localhost:3001'
         />
       )}
     </div>
